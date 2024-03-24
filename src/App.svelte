e3fab28040a8512c0b38d237713c24e9ae928a24
tactkimura <tactkimura_develop@icloud.com>
2024-03-25 01:30:24 +0900

    フォーム作成

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,7 @@
   import { onMount } from 'svelte';
   import liff from "@line/liff";
   import Textfield from '@smui/textfield';
-  import {useMutation} from '@apollo/client';
-  import { gql } from '@apollo/client/core';
+  import axios from 'axios';
 
   async function init() {
     return await liff.init({
@@ -26,32 +25,29 @@
 
   let formData = '';
 
-  const INSERT_DATA = gql`
-    mutation MyMutation($lineUuid: String!, $data: String!) {
-      insert_user_one(object: {data: $data, line_uuid: $lineUuid}) {
-        id
-      }
-    }
-  `;
-
-  const [insertData] = useMutation(INSERT_DATA, {
-    context: {
-      uri: 'https://huge-frog-75.hasura.app/v1/graphql',
-      headers: {
-        'x-hasura-admin-secret': import.meta.env.HASURA_ADMIN_SECRET
-      }
-    }
-  });
 
   const handleSubmit = async () => {
     try {
-      await insertData({
-        variables: {
-          lineUuid: user.id,
-          data: formData
+      const response = await axios.post(
+        'https://huge-frog-75.hasura.app/v1/graphql',
+        {
+          query: `
+            mutation InsertData($lineUuid: String!, $data: String!) {
+              insert_todos_one(object: { uuid: $lineUuid, data: $data }) {
+                id
+              }
+            }
+          `,
+          variables: {
+            lineUuid: user.uuid,
+            data: formData
+          },
+          headers: {
+            'x-hasura-admin-secret': import.meta.env.HASURA_ADMIN_SECRET
+          }
         }
-      });
-    } catch(error) {
+      )
+    } catch (error) {
       console.error(error);
     }
   }
@@ -72,11 +68,11 @@
           <Textfield textarea bind:value={formData}></Textfield>
           <button on:click={handleSubmit}>送信</button>
         </div>
-      {/if}
         <p>ログイン済</p>
-      {:else}
-        <p>スマホアプリでLINEを開いてください</p>
       {/if}
+    {:else}
+      <p>スマホアプリでLINEを開いてください</p>
+    {/if}
   {:catch e}
     <p>LIFF init failed.</p>
     <p><code>{`${e}`}</code></p>
